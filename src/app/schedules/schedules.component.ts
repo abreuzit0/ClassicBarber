9302c516cfbc97a832d8a7374f404501891a0c0d
abreuzit0 <lucasabreuthree@gmail.com>
2023-09-15 21:26:28 -0300

    Revert "ajustes"
    This reverts commit b8e445553b7fc1dfe65e624750a2fe87629cf67d.

--- src/app/schedules/schedules.component.ts
+++ src/app/schedules/schedules.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -16,4 +16,5 @@ export class SchedulesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 }
